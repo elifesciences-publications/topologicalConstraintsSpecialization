00c81ac6df9755c7ead8db344dc9c4c03b5e29c1
بندر لڑکا <Davidyanni1406@gmail.com>
2020-05-05 23:56:16 -0400

    fixed bipartite build_c bug
    thanks @pablo for pointing this out!

--- shanesProj.py
+++ shanesProj.py
@@ -11,14 +11,18 @@ from scipy.linalg import circulant
 from scipy.optimize import minimize
 from matplotlib import pyplot as plt
 from matplotlib import collections  as mc
+import multiprocessing as mp
+import pandas as pd
+from tqdm import tqdm
 
 #%%
 def build_c(beta, N, mode = 'bipartite'):
     
     if mode=='bipartite':
-        A = circulant([i%2 for i in range(1,N+1)])
+        A = circulant([i%2 for i in range(N)])
         one = np.ones(N)
-        t1 = beta*(A*np.outer(1/np.dot(one,A),one))
+        A = A + np.eye(N)
+        t1 = beta*(A / A.sum(axis=1))
         t2 = (1-beta)*np.eye(N)
         return(t1+t2)
     if mode=='neighbor':
@@ -52,15 +56,12 @@ def W(v,c, alpha):
     return(np.sum(((v**alpha)@c)*((1-v)**alpha), axis=v.ndim-1))
 
 """
-Hi David,
-
-First, no worries, next week will be great!
-
-Great point on the equal number confusion. At a minimum, they have missed how general our model is.
-
-I had another idea regarding how to model the 1:2 ratio. What if we cap how much viability a cell can use? Cells can still make up to 1, but now can only use, say 0.3. Any amount they have above that is just wasted. This seems biologically relevant, too (for example, we need vitamin D, but taking supplements with 1000X what we need in a day doesn't make us extra healthy).
-
-Best,
+Here are a few small things:
+1. Comments / edits on response to comment 2.
+2. Can you transpose the heatmap for the star topology so beta is on the y-axis and alpha is on the x-axis?
+3. Can you make alpha-beta heat maps for the viability limiting cases? One each for fully connected and bipartite topologies, please!
+4. Could you also run this for viability limited to 0.5 (and again make heat maps)?
+5. Also, can you make the configuration images with white backgrounds and thicker connecting lines (like the images you sent on April 1)?
 """
 
 
@@ -137,12 +138,20 @@ def spec(v):
 def make_network_for_plotting(N=10, mode='bipartite'):
     theta_step = 2*np.pi/(N)
     thetas = [theta_step*i for i in range(N)]
+    if mode == 'star':
+        theta_step = 2*np.pi/(N-1)
+        thetas = [theta_step*i for i in range(N-1)]
     pts = []
+    if mode == 'star':
+        pts.append((0,0))
     for theta in thetas:
         pts.append( (np.cos(theta), np.sin(theta)) )
 
     lines = []
     for i,pt in enumerate(pts):
+        if mode == 'star':
+            opt = ((0,0))
+            lines.append([pt, opt])
         for j,opt in enumerate(pts):
             if mode == 'full':
                 lines.append([pt,opt])
@@ -152,22 +161,22 @@ def make_network_for_plotting(N=10, mode='bipartite'):
             elif mode == 'neighbor':
                 if (j==(i+1)%N):
                     lines.append([pt, opt])
+
     return(np.array(pts), lines)
 
 def make_colorized(v, N=10, mode='bipartite'):
-    with plt.style.context('dark_background'):
-        pts, lines = make_network_for_plotting(N=N, mode=mode)
-        fig, ax = plt.subplots(figsize = (10,10))
-        lc = mc.LineCollection(lines, colors='w', linewidths = .5)
-        ax.add_collection(lc)
-        ax.scatter(pts[:,0],pts[:,1],
-            c=[plt.cm.bwr(1-vi) for vi in v], s=1500, edgecolor='w')
-        ax.autoscale()
-        fig2,ax2 = plt.subplots()
-        cb = ax2.imshow(np.stack([np.linspace(min(v), max(v), 50) for i in range(6)]).T,
-            cmap=plt.cm.bwr)
-        fig2.colorbar(cb, ax=ax2)
-        return(fig,fig2)
+    pts, lines = make_network_for_plotting(N=N, mode=mode)
+    fig, ax = plt.subplots(figsize = (10,10))
+    lc = mc.LineCollection(lines, colors='k', linewidths = 3.5)
+    ax.add_collection(lc)
+    ax.scatter(pts[:,0],pts[:,1],
+        c=[plt.cm.bwr(1-vi) for vi in v], s=1500, edgecolor='w')
+    ax.autoscale()
+    fig2,ax2 = plt.subplots()
+    cb = ax2.imshow(np.stack([np.linspace(min(v), max(v), 50) for i in range(6)]).T,
+        cmap=plt.cm.bwr)
+    fig2.colorbar(cb, ax=ax2)
+    return(fig,fig2)
 
 def evo(pop, c, alpha, nsteps, limited=False):
     N = pop.shape[0]
@@ -186,17 +195,79 @@ def evo(pop, c, alpha, nsteps, limited=False):
         #print(f"ep: {t}, fit: {np.average(fits)}")
     return(pop)
 
-def make_hmap(mode, nsteps=2000, popsize=10000, limited=False):
-    alphas = np.linspace(.5,1.0,6)
+def make_hmap(mode, nsteps, popsize, limited):
+    alphas = np.linspace(.1,1,10)
     betas = np.linspace(0,1,11)
-    hm = np.zeros((6,11))
-    for i,alpha in enumerate(alphas):
+    hm = np.zeros((10,11))
+    print(f"mode: {mode} limit: {limited}")
+    for i,alpha in tqdm(enumerate(alphas)):
         for j,beta in enumerate(betas):
             c = build_c(beta, 10, mode=mode)
-            print(f"alpha: {alpha}, beta: {beta}")
+            #print(f"mode:{mode} limit:{limited} alpha: {alpha}, beta: {beta}")
             pop = np.random.rand(popsize,10)
             pop = evo(pop, c, alpha, nsteps, limited)
             hm[i,j] = np.average([spec(v) for v in pop])
-    return(hm)
+    return((mode, limited, hm))
+
+
+def mh(x):
+    return(make_hmap(*x))
+
+def make_hmaps_par(modes, limits, nsteps=2000, popsizes=10000):
+    situations = []
+    for mode in modes:
+        for lim in limits:
+            situations.append((mode,nsteps, popsizes, lim))
+    with mp.Pool(6) as pool:
+        res = pool.starmap(make_hmap, situations)
+        return([i for i in res])
+
+def make_hmap_figure_from_data(fname):
+    df = pd.read_csv(fname).drop('Unnamed: 0', axis=1)
+    fig, ax = plt.subplots(figsize=(6,8))
+    ax.imshow(np.rot90(df))
+    linspace = np.linspace(1,0,11)
+    rounded = [round(x,2) for x in linspace]
+    ax.set_yticklabels(rounded, fontsize=14)
+    ax.set_yticks(np.arange(11))
+    linspace = np.linspace(.1,1,10)
+    rounded = [round(x,2) for x in linspace]
+    ax.set_xticklabels(rounded, fontsize=14)
+    ax.set_xticks(np.arange(10))
+    ax.set_xlabel(r"$\alpha$", fontsize=18)
+    ax.set_ylabel(r"$\beta$", fontsize=18)
+    to_save_fname = fname.split('.')
+    if len(to_save_fname)>2:
+        to_save_fname = to_save_fname[0] + to_save_fname[1]
+    else:
+        to_save_fname = to_save_fname[0]
+    to_save_fname = to_save_fname + "_figure.pdf"
+    fig.savefig(to_save_fname)
+
+def star_analysis_plot():
+    n = np.linspace(1,20,1000)
+    num = 2*n**2
+    denom = num + 2*n + 8
+    res = -.5*np.log2(num/denom)
+    fig, ax = plt.subplots(figsize=(8,6))
+    ax.plot(n, res, 'b', lw=3)
+    ax.grid(lw=.8, c='gray')
+    ax.set_xlabel("Number of individuals", fontsize=16)
+    ax.set_ylabel(r"$\alpha_{max}$", fontsize=16)
+    ax.set_ylim(0,1)
+    ax.set_xticklabels(ax.get_xticks(), fontsize=14)
+    ax.set_yticklabels(np.round(ax.get_yticks(),2), fontsize=14)
+    return fig
+
+if __name__ == '__main__':
+    modes = ['bipartite', 'full', 'neighbor', 'star']
+    limits = [0.2, 0.5, False]
+    results = make_hmaps_par(modes, limits)
+    for result in results:
+        fname = f"{result[0]}_{result[1]}.csv"
+        df = pd.DataFrame(result[2])
+        df.to_csv(fname)
+        make_hmap_figure_from_data(fname)
+
 
 
